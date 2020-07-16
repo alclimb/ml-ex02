b4a6c18eef7d5a620492fcf9123d2d934cba70ef
hikaru <alclimb@users.noreply.github.com>
2020-07-16 09:40:59 +0900

    feat: 評価基準を調整

--- Assets/FunnelAgent.cs
+++ Assets/FunnelAgent.cs
@@ -36,8 +36,10 @@ public class FunnelAgent : Agent
 
     public override void OnEpisodeBegin()
     {
+        const float range = 5f;
+
         // 位置を初期化
-        this._rigidbody.position = Vector3.zero + this._basePosition;
+        this._rigidbody.position = this._basePosition + new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
         this._rigidbody.velocity = Vector3.zero;
         this._rigidbody.rotation = Quaternion.identity;
         this._rigidbody.angularVelocity = Vector3.zero;
@@ -51,6 +53,9 @@ public class FunnelAgent : Agent
     {
         const float power = 300f;
         const float area = 10f;
+        const float limitTime = 100f; // リミット時間 (単位: 秒)
+
+        var episodeTime = (float)this._episodeTime.Elapsed.TotalSeconds;
 
         var force = new Vector3(vectorAction[0], vectorAction[1], vectorAction[2]) * power;
 
@@ -60,8 +65,15 @@ public class FunnelAgent : Agent
         // ベース地点からの移動距離を算出
         var distance = Vector3.Distance(this._rigidbody.position, this._basePosition);
 
-        // 評価: 報酬を与える (少ないエネルギー程高得点)
-        base.AddReward((power - Vector3.Distance(force, Vector3.zero)) / power);
+        // // 評価: 報酬を与える (少ないエネルギー程高得点)
+        // base.AddReward((power - Vector3.Distance(force, Vector3.zero)) / power);
+
+        // リミット時間を判定
+        if (limitTime < episodeTime)
+        {
+            // リセットして次のエピソードを開始
+            base.EndEpisode();
+        }
 
         // エリア外に移動した場合
         if (area < distance)
@@ -72,10 +84,17 @@ public class FunnelAgent : Agent
             // リセットして次のエピソードを開始
             base.EndEpisode();
         }
-        else
+
+        // ベース地点の範囲内に入った場合
+        else if (distance < 0.5f)
         {
+            // MEMO: 評価ポイント
+            // 中心に位置に、素早く、少ないエネルギーで
+
+            var score = (limitTime - episodeTime) * 0.1f;
+
             // 評価: 報酬を与える (ベース地点に近ければ高得点)
-            base.AddReward((area - distance) / area);
+            base.AddReward(score);
         }
 
         // UI更新
@@ -86,6 +105,6 @@ public class FunnelAgent : Agent
     {
         sensor.AddObservation(this._rigidbody.position - this._basePosition);
         sensor.AddObservation(this._rigidbody.velocity);
-        // sensor.AddObservation((float)this._episodeTime.Elapsed.TotalSeconds);
+        sensor.AddObservation((float)this._episodeTime.Elapsed.TotalSeconds);
     }
 }
