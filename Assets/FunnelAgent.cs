619fac9d5f89094452d7c6106383fb1787eee123
hikaru <alclimb@users.noreply.github.com>
2020-07-20 16:09:44 +0900

    feat: 評価基準を改善

--- Assets/FunnelAgent.cs
+++ Assets/FunnelAgent.cs
@@ -12,11 +12,17 @@ public class FunnelAgent : Agent
     /// <summary>基準になる位置</summary>
     private Vector3 _basePosition;
 
-    /// <summary>エピソード毎のストップウォッチ</summary>
-    private System.Diagnostics.Stopwatch _episodeTime = new System.Diagnostics.Stopwatch();
+    // /// <summary>エピソード毎のストップウォッチ</summary>
+    // private System.Diagnostics.Stopwatch _episodeTime = new System.Diagnostics.Stopwatch();
 
     [Header("ステータス表示用テキスト")]
     public UnityEngine.UI.Text StatusText;
+
+    /// <summary>
+    /// 向き
+    /// </summary>
+    [Header("向き")]
+    public Quaternion Direction = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
@@ -44,22 +50,22 @@ public class FunnelAgent : Agent
         this._rigidbody.rotation = Quaternion.identity;
         this._rigidbody.angularVelocity = Vector3.zero;
 
-        // ストップウォッチをリセット
-        this._episodeTime.Reset();
-        this._episodeTime.Start();
+        // // ストップウォッチをリセット
+        // this._episodeTime.Reset();
+        // this._episodeTime.Start();
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
         const float power = 300f;
         const float area = 10f;
-        const float limitTime = 5f; // リミット時間 (単位: 秒)
+        // const float limitTime = 10f; // リミット時間 (単位: 秒)
 
         float actionX = vectorAction[0];
         float actionY = vectorAction[1];
         float actionZ = vectorAction[2];
 
-        var episodeTime = (float)this._episodeTime.Elapsed.TotalSeconds;
+        // var episodeTime = (float)this._episodeTime.Elapsed.TotalSeconds;
 
         var force = new Vector3(actionX, actionY, actionZ) * power;
 
@@ -72,12 +78,12 @@ public class FunnelAgent : Agent
         // // 評価: 報酬を与える (少ないエネルギー程高得点)
         // base.AddReward((power - Vector3.Distance(force, Vector3.zero)) / power);
 
-        // リミット時間を判定
-        if (limitTime < episodeTime)
-        {
-            // リセットして次のエピソードを開始
-            base.EndEpisode();
-        }
+        // // リミット時間を判定
+        // if (limitTime < episodeTime)
+        // {
+        //     // リセットして次のエピソードを開始
+        //     base.EndEpisode();
+        // }
 
         // エリア外に移動した場合
         if (area < distance)
@@ -89,17 +95,27 @@ public class FunnelAgent : Agent
             base.EndEpisode();
         }
 
-        // ベース地点の範囲内に入った場合
-        else if (distance < 0.5f)
-        {
-            // MEMO: 評価ポイント
-            // 中心に位置に、素早く、少ないエネルギーで
+        // // ベース地点の範囲内に入った場合
+        // else if (distance < 0.5f)
+        // {
+        //     // MEMO: 評価ポイント
+        //     // 中心に位置に、素早く、少ないエネルギーで
 
-            var score = (limitTime - episodeTime) * 0.1f;
+        //     var score = (limitTime - episodeTime) * 0.1f;
 
-            // 評価: 報酬を与える (ベース地点に近ければ高得点)
-            base.AddReward(score);
-        }
+        //     // 評価: 報酬を与える (ベース地点に近ければ高得点)
+        //     base.AddReward(score);
+        // }
+
+        // 評価: 報酬を与える (ベース地点に近ければ高得点)
+        base.AddReward(0.001f * (area - distance));
+
+        // 向きの報酬
+        base.AddReward(0.01f * Vector3.Dot(this._rigidbody.transform.forward, new Vector3(0f, 0f, 1f)));
+        base.AddReward(0.01f * Vector3.Dot(this._rigidbody.transform.up, new Vector3(0f, 1f, 0f)));
+
+        // タイムペナルティ
+        base.AddReward(-0.001f);
 
         // アフターバーナー表現
         {
@@ -153,9 +169,17 @@ public class FunnelAgent : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
+        // 自身の基準からの相対位置
         sensor.AddObservation(this._rigidbody.position - this._basePosition);
+
+        // 自身の速度
         sensor.AddObservation(this._rigidbody.velocity);
-        sensor.AddObservation((float)this._episodeTime.Elapsed.TotalSeconds);
+
+        // 自身の向き
+        sensor.AddObservation(this._rigidbody.rotation);
+
+        // 経過時間
+        // sensor.AddObservation((float)this._episodeTime.Elapsed.TotalSeconds);
     }
 
     public void Reset()
